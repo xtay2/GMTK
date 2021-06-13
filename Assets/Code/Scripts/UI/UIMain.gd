68ad57a1277e831e1bb3d99b5a3042d00dd7d17b
xtay2 <x-tay2@o2mail.de>
2021-06-13 17:38:22 +0200

    f

--- Assets/Code/Scripts/UI/UIMain.gd
+++ Assets/Code/Scripts/UI/UIMain.gd
@@ -17,15 +17,10 @@ func _ready():
 func update_enemy_info(wave, enemy_count):
 	if main.wave != wave:
 		main.wave = wave
-		max_enemy_count = enemy_count
 		
-	$EnemyStateInfo/Enemies.text = "%s | %s" % [enemy_count, max_enemy_count]
+	$EnemyStateInfo/Enemies.text = "Enemies left: " + String(enemy_count)
 	$EnemyStateInfo/Wave.text = "%s" % main.wave
 	
-	var music = music_wave[wave - 1]
-	if music != $MusicPlayer.stream:
-		$MusicPlayer.stream = music
-#		$MusicPlayer.play(0)
 
 func _on_Upgrade_pressed():
 	emit_signal("on_upgrade")
